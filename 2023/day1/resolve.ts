1217e181730ac24f3eacacfd68824a7038d7ea42
fprudhomme <fprudhomme@knowmore.fr>
2023-12-02 09:30:20 +0100

    AoC 2023 - Day 1

---
+++ 2023/day1/resolve.ts
@@ -0,0 +1,53 @@
+const file = Bun.file(import.meta.dir + '/input.txt'); // BunFile
+const content = await file.text();
+
+function part1() {
+    let lines = content.split("\n");
+    let sum = 0; 
+    for (let line of lines) {
+        var ptrn = /[0-9]/mg;
+
+        var match;
+        let matched = []
+        while ((match = ptrn.exec(line)) != null) {
+            matched.push(match)
+        }
+        sum += Number(matched[0][0] + matched[matched.length - 1][0])
+    }
+    return sum;
+}
+
+function part2() {
+    const map: Record<string, string> = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    let lines = content.split("\n");
+    let sum = 0;
+    
+    for (let line of lines) {
+        let modifiedLine = line;
+        for (let key in map) {
+            modifiedLine = modifiedLine.replaceAll(key, key + map[key] + key);
+        }
+        var ptrn = /[0-9]/mg;
+
+        var match;
+        let matched = []
+        while ((match = ptrn.exec(modifiedLine)) != null) {
+            matched.push(match)
+        }
+        sum += Number(matched[0][0] + matched[matched.length - 1][0])
+    }
+    return sum;
+}
+
+console.log("Part 1 : " + part1());
+console.log("Part 2 : " + part2());
