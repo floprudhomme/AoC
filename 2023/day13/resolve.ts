3c8d25d908e8223783dccab155a9e6afb346beae
Florian Prudhomme <fprudhomme@knowmore.fr>
2023-12-15 09:24:47 +0100

    AoC 2023 - Day 13 - Part 2

--- 2023/day13/resolve.ts
+++ 2023/day13/resolve.ts
@@ -10,7 +10,6 @@ function part1() {
         let j = 0;
         for (let i = 0 ; i < bloc[0].length - 1 ; i++) {
             if (bloc[j][i] === bloc[j][i + 1]) {
-                // console.log("line ", j, " :  column ", i, " is equal to column ", i + 1 , " (", bloc[j][i],bloc[j][i + 1],")");
                 let isSymetric = true;
                 while (j < bloc.length) {
                     if (bloc[j][i] !== bloc[j][i + 1]) {
@@ -45,5 +44,50 @@ function part1() {
     return sum;
 }
 
+
+function part2() {
+
+    function findSymetry(bloc: string[][]): number {
+        let j = 0;
+        for (let i = 0 ; i < bloc[0].length - 1 ; i++) {
+            let isSymetric = 0;
+            while (j < bloc.length) {
+                if (bloc[j][i] !== bloc[j][i + 1]) {
+                    isSymetric++;
+                }
+                if (isSymetric > 1) {
+                    break;
+                }
+                j++;
+            }
+            
+            if (isSymetric <= 1) {
+                isSymetric = 0;
+                for (let y = 0 ; y < bloc.length ; y++) {
+                    for (let left = i, right = i + 1 ; left >= 0 && right < bloc[y].length ; left--, right++) {
+                        if (bloc[y][left] !== bloc[y][right]) {
+                            isSymetric++;
+                        }
+                    }
+                }
+                if (isSymetric == 1) {
+                    return i + 1;
+                }
+            }
+            j = 0;
+        }
+        return 0;
+    }
+    let sum = 0;
+    let blocs = content.split("\n\n").map(elt => elt.split("\n").map(elt2 => elt2.split("")));
+
+    for (let currentBloc of blocs) {
+        sum += findSymetry(currentBloc);
+        sum += findSymetry(currentBloc[0].map((_, colIndex) => currentBloc.map(row => row[colIndex]))) * 100;
+    }
+    return sum;
+}
+
 console.log("Part 1 : " + part1());
+console.log("Part 2 : " + part2());
 
