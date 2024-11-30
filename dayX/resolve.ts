633f6031ab6daaa16ede0a6feb5b7d963dfeaa7f
floprudhomme <fprudhomme@knowmore.fr>
2024-11-30 16:04:52 +0100

    add file to copy for init exercice

---
+++ dayX/resolve.ts
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+const content = await fs.promises.readFile("./input.txt", {
+  encoding: "utf-8",
+});
+
+function part1() {
+    return 0;
+}
+
+function part2() {
+    return 0;
+}
+
+console.log("Part 1 : " + part1());
+console.log("Part 2 : " + part2());
