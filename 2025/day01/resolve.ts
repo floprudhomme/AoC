69212295c8a2a05b6660f39773d30bb90b116afd
floprudhomme <fprudhomme@knowmore.fr>
2025-12-01 14:56:18 +0100

    AoC 2025 - Day 01 (Part 2)

--- 2025/day01/resolve.ts
+++ 2025/day01/resolve.ts
@@ -5,27 +5,63 @@ const content = await fs.promises.readFile("./input.txt", {
 });
 
 function part1() {
-    const lines = content.split("\n");
-    let current = 50;
-    let total = 0;
-    for (const line of lines) {
-        const direction = line[0];
-        const change = parseInt(line.slice(1));
-        if (direction === "L") {
-            current -= change;
-        } else if (direction === "R") {
-            current += change;
-        }
-        current = current % 100;
-        if (current === 0) {
-            total++;
-        }
+  const lines = content.split("\n");
+  let current = 50;
+  let total = 0;
+  for (const line of lines) {
+    const direction = line[0];
+    const change = parseInt(line.slice(1));
+    if (direction === "L") {
+      current -= change;
+    } else if (direction === "R") {
+      current += change;
     }
-    return total;
+    current = current % 100;
+    if (current === 0) {
+      total++;
+    }
+  }
+  return total;
 }
 
 function part2() {
-    return 0;
+  let current = 50;
+  let total = 0;
+
+  for (const line of content.split("\n")) {
+    if (line.startsWith("L")) {
+      const change = parseInt(line.slice(1), 10);
+      const newPos = current - change;
+
+      let delta = current % 100;
+      if (delta === 0) {
+        delta = 100;
+      }
+      if (change >= delta) {
+        total += 1 + Math.floor((change - delta) / 100);
+      }
+
+      current = sanitize(newPos);
+    } else if (line.startsWith("R")) {
+      const change = parseInt(line.slice(1), 10);
+      const newPos = current + change;
+
+      total += Math.floor((current + change) / 100);
+
+      current = sanitize(newPos);
+    }
+  }
+
+  return total;
+
+  function sanitize(dial: number): number {
+    if (dial < 0) {
+      return sanitize(100 + dial);
+    } else if (dial > 99) {
+      return sanitize(dial - 100);
+    }
+    return dial;
+  }
 }
 
 console.log("Part 1 : " + part1());
